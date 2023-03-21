0bf1069eecb973915ec12194a6fdbcc5a6cd09d9
Viktor Edman <viktor@edman.se>
2023-03-21 02:46:28 +0100

    got password generation and sign in working

--- src/lib/db.mjs
+++ src/lib/db.mjs
@@ -7,10 +7,10 @@ dotenv.config()
 
 const ADMINPASSWORD = process.env.GSSC_ADMIN_PASSWORD || Math.random().toString(36).slice(-8)
 const DBCONFIG = {
-    user: process.env.POSTGRES_USER,
+    user: process.env.POSTGRES_USER || "gssc",
     host: process.env.POSTGRES_HOST || "localhost",
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE || "gssc",
+    password: process.env.POSTGRES_PASSWORD || "gssc",
     port: process.env.POSTGRES_PORT || "5432", 
     max: 10,
     idleTimeoutMillis: 1000,
@@ -55,12 +55,24 @@ const deleteUser = async (id) => {
     return res
 }
 
+const updateUserPassword = async (id, password) => {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const SQL = `UPDATE users SET password = '${hashedPassword}' WHERE id = ${id};`
+    console.log(SQL)
+    const res = await pool.query(SQL)
+    return res
+}
+
 const initDb = async () => {
     await createUserTable()
     const adminUser = await getUserByName('admin')
     if (adminUser) {
         console.log("Admin already exists") 
         console.log("Not creating admin account")
+        if (!(await bcrypt.compare(ADMINPASSWORD, adminUser.password))) {
+            console.log("Updating admin password to", ADMINPASSWORD)
+            await updateUserPassword(adminUser.id, ADMINPASSWORD)
+        }
     } else {
         console.log("Admin does not exist")
         console.log("Creating admin account with password " + ADMINPASSWORD)
--- src/pages/api/secretData.js
+++ src/pages/api/secretData.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-    res.status(200).json({response: "this is allowed"})
+import { getServerSession } from "next-auth/next"
+import { authOptions } from './auth/[...nextauth]'
+
+export default async function handler(req, res) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) res.status(401).json({response: "forbidden, come back when you sign in"})
+    if (session) res.status(200).json({response: "this is allowed"})
 }
