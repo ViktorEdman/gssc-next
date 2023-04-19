4d59461739af0c44af4367585417e94df00b96f8
Viktor Edman <viktor@edman.se>
2023-04-19 10:23:35 +0200

    added click by enter/space

--- src/components/ServerInfo.tsx
+++ src/components/ServerInfo.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function ServerInfo({ server }) {
-    const { name, map, password } = server
-    const [showMore, setShowMore] = useState(false)
+    const { name, map, password } = server;
+    const [showMore, setShowMore] = useState(false);
+
+    const toggleShowMore = () => {
+        setShowMore(!showMore);
+    };
 
     return server.error ? (
-        <li className="px-2">
-            <span className="red dot mr-2" />
-            <span className="text-stone-100/50">{server.game} is DOWN</span>
+        <li className='px-2'>
+            <span className='red dot mr-2' />
+            <span className='text-stone-100/50'>{server.game} is DOWN</span>
         </li>
     ) : (
         <li
-            className="bg-black/40 max-w-md my-2 py-2 px-2 rounded-2xl cursor-pointer"
+            className='bg-black/40 max-w-md my-2 py-2 px-2 rounded-2xl cursor-pointer'
             onClick={(e) => {
-                e.preventDefault()
-                setShowMore(!showMore)
+                e.preventDefault();
+                toggleShowMore();
             }}
+            onKeyDown={(e) => {
+                if (e.key === "enter" || e.key === " ") {
+                    toggleShowMore();
+                }
+            }}
+            tabIndex={0}
         >
-            <span className="green dot mr-2" />
-            <span className=" text-stone-100	">
+            <span className='green dot mr-2' />
+            <span className=' text-stone-100	'>
                 {server.game} is UP with {server.players.length}/
                 {server.maxplayers} players
-                <span className="float-right">{showMore ? '-' : '+'}</span>
+                <span className='float-right'>{showMore ? "-" : "+"}</span>
             </span>
 
             {showMore ? (
-                <ul className="text-white/80 ml-8">
+                <ul className='text-white/80 ml-8'>
                     <li>Server name: {name}</li>
                     {map.length > 0 ? <li>Map: {map}</li> : null}
-                    <li>Password protected: {password ? 'yes' : 'no'}</li>
+                    <li>Password protected: {password ? "yes" : "no"}</li>
                     {server.raw.version ? (
                         <li>Server version: {server.raw.version}</li>
                     ) : null}
@@ -36,8 +46,8 @@ function ServerInfo({ server }) {
                         <li>
                             <a
                                 href={`https://store.steampowered.com/app/${server.raw.appId}`}
-                                className="hover:underline text-blue-500 visited:text-purple-800"
-                                target="_blank"
+                                className='hover:underline text-blue-500 visited:text-purple-800'
+                                target='_blank'
                             >
                                 Buy on steam!
                             </a>
@@ -45,10 +55,10 @@ function ServerInfo({ server }) {
                     ) : null}
                 </ul>
             ) : (
-                ''
+                ""
             )}
         </li>
-    )
+    );
 }
 
-export default ServerInfo
+export default ServerInfo;
